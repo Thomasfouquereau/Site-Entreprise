88c1c456af95f67576e0892ce4df9eb92cb2324c
thomas fouquereau <thomas.fouquereau@gmail.com>
2024-07-18 04:27:23 +0200

    add animation 42dev page

--- site entreprise/src/42dev/42dev.jsx
+++ site entreprise/src/42dev/42dev.jsx
@@ -1,5 +1,6 @@
+import React, { useEffect } from 'react';
+import gsap from 'gsap';
 import styled, { keyframes } from "styled-components"
-
 import logo_42dev from "../assets/logo-42dev-no-bg.svg"
 
 const theme = {
@@ -13,644 +14,140 @@ const Container = styled.div`
     font-family: 'Montserrat', sans-serif;
 `
 
-const Animation1 = keyframes`
- 0% {
-        content: "_";
-    }
-    1%{
-        content: "4_";
-    }
-    2% {
-        content: "42_";
-    }
-    3% {
-        content: "42,_";
-    }
-    4% {
-        content: "42, c'_";
-    }
-    5% {
-        content: "42, c'es_";
-    }
-    6% {
-        content: "42, c'est_";
-    }
-    7% {
-        content: "42, c'est l_";
-    }
-    8% {
-        content: "42, c'est la_";
-    }
-    9% {
-        content: "42, c'est la r_";
-    }
-    10% {
-        content: "42, c'est la ré_";
-    }
-    11% {
-        content: "42, c'est la rép_";
-    }
-    12% {
-        content: "42, c'est la répo_";
-    }
-    13% {
-        content: "42, c'est la répon_";
-    }
-    14% {
-        content: "42, c'est la réponse_";
-    }
-    16% {
-        content: "42, c'est la réponse à_";
-    }
-    17% {
-        content: "42, c'est la réponse à t_";
-    }
-    18% {
-        content: "42, c'est la réponse à to_";
-    }
-    19% {
-        content: "42, c'est la réponse à tou_";
-    }
-    20% {
-        content: "42, c'est la réponse à tou _";
-    }
-    24% {
-        content: "42, c'est la réponse à tout_";
-    }
-    25% {
-        content: "42, c'est la réponse à toute_";
-    }
-    27% {
-        content: "42, c'est la réponse à toutes_";
-    }
-    28% {
-        content: "42, c'est la réponse à toutes _";
-    }
-    31% {
-        content: "42, c'est la réponse à toutes l_";
-    }
-    32% {
-        content: "42, c'est la réponse à toutes le_";
-    }
-    33% {
-        content: "42, c'est la réponse à toutes les_";
-    }   
-    35% {
-        content: "42, c'est la réponse à toutes les _";
-    }
-    37% {
-        content: "42, c'est la réponse à toutes les q_";
-    }
-    38% {
-        content: "42, c'est la réponse à toutes les qu_";
-    }
-    39% {
-        content: "42, c'est la réponse à toutes les que_";
-    }
-    41% {
-        content: "42, c'est la réponse à toutes les ques_";
-    }
-    44% {
-        content: "42, c'est la réponse à toutes les quest_";
-    }
-    45% {
-        content: "42, c'est la réponse à toutes les questi_";
-    }
-    46% {
-        content: "42, c'est la réponse à toutes les questio_";
-    }
-    47% {
-        content: "42, c'est la réponse à toutes les question_";
-    }
-    49% {
-        content: "42, c'est la réponse à toutes les questions_";
-    }
-    51% {
-        content: "42, c'est la réponse à toutes les questions,_";
-    }
-    53% {
-        content: "42, c'est la réponse à toutes les questions, _";
-    }
-    54% {
-        content: "42, c'est la réponse à toutes les questions, n_";
-    }
-    55% {
-        content: "42, c'est la réponse à toutes les questions, ne_";
-    }
-    56% {
-        content: "42, c'est la réponse à toutes les questions, nes_";
-    }
-    60% {
-        content: "42, c'est la réponse à toutes les questions, ne_";
-    }
-    61% {
-        content: "42, c'est la réponse à toutes les questions, n_";
-    }
-    62% {
-        content: "42, c'est la réponse à toutes les questions, n'_";
-    }
-    63% {
-        content: "42, c'est la réponse à toutes les questions, n'e_";
-    }
-    64% {
-        content: "42, c'est la réponse à toutes les questions, n'es_";
-    }
-    65% {
-        content: "42, c'est la réponse à toutes les questions, n'est_";
-    }
-    67% {
-        content: "42, c'est la réponse à toutes les questions, n'est-_";
-    }
-    68% {
-        content: "42, c'est la réponse à toutes les questions, n'est-c_";
-    }
-    71% {
-        content: "42, c'est la réponse à toutes les questions, n'est-ce_";
-    }
-    74% {
-        content: "42, c'est la réponse à toutes les questions, n'est-ce_";
-    }
-    76% {
-        content: "42, c'est la réponse à toutes les questions, n'est-ce _";
-    }
-    77% {
-        content: "42, c'est la réponse à toutes les questions, n'est-ce p_";
-    }
-    78% {
-        content: "42, c'est la réponse à toutes les questions, n'est-ce p_";
-    }
-    80% {
-        content: "42, c'est la réponse à toutes les questions, n'est-ce pa_";
-    }
-    83% {
-        content: "42, c'est la réponse à toutes les questions, n'est-ce pas_";
-    }
-    86% {
-        content: "42, c'est la réponse à toutes les questions, n'est-ce pas!_";
-    }
-    87% {
-        content: "42, c'est la réponse à toutes les questions, n'est-ce pas!!_";
-    }
-    88% {
-        content: "42, c'est la réponse à toutes les questions, n'est-ce pas!!!_";
-    }
-    90% {
-        content: "42, c'est la réponse à toutes les questions, n'est-ce pas!!!_";
-    }
-    93% {
-        content: "42, c'est la réponse à toutes les questions, n'est-ce pas!!_";
-    }
-    95% {
-        content: "42, c'est la réponse à toutes les questions, n'est-ce pas!_";
-    }
-    97% {
-        content: "42, c'est la réponse à toutes les questions, n'est-ce pas_";
-    }
-    100% {
-        content: "42, c'est la réponse à toutes les questions, n'est-ce pas?_";
-    }
-`
-
-const Animation2 = keyframes`
-    0% {
-        content: "42, c'est la réponse à toutes les questions, n'est-ce pas?_";
-    }
-    10% {
-        content: "42, c'est la réponse à toutes les questions, n'est-ce pas_";
-    }
-    11% {
-        content: "42, c'est la réponse à toutes les questions, n'est-ce pas_";
-    }
-    12% {
-        content: "42, c'est la réponse à toutes les questions, n'est-ce p_";
-    }
-    13% {
-        content: "42, c'est la réponse à toutes les questions, n'est-ce_";
-    }
-    14% {
-        content: "42, c'est la réponse à toutes les questions, n'est-_";
-    }
-    15% {
-        content: "42, c'est la réponse à toutes les questions, n'es_";
-    }
-    16% {
-        content: "42, c'est la réponse à toutes les questions, n'e_";
-    }
-    17% {
-        content: "42, c'est la réponse à toutes les questions, n_";
-    }
-    18% {
-        content: "42, c'est la réponse à toutes les questions, _";
-    }
-    19% {
-        content: "42, c'est la réponse à toutes les questions,_";
-    }
-    20% {
-        content: "42, c'est la réponse à toutes les questions_";
-    }
-    21% {
-        content: "42, c'est la réponse à toutes les questio_";
-    }
-    22% {
-        content: "42, c'est la réponse à toutes les quest_";
-    }
-    23% {
-        content: "42, c'est la réponse à toutes les ques_";
-    }
-    24% {
-        content: "42, c'est la réponse à toutes les que_";
-    }
-    25% {
-        content: "42, c'est la réponse à toutes les qu_";
-    }
-    26% {
-        content: "42, c'est la réponse à toutes les q_";
-    }
-    27% {
-        content: "42, c'est la réponse à toutes les _";
-    }
-    28% {
-        content: "42, c'est la réponse à toutes le_";
-    }
-    29% {
-        content: "42, c'est la réponse à toutes l_";
-    }
-    30% {
-        content: "42, c'est la réponse à toutes _";
-    }
-    31% {
-        content: "42, c'est la réponse à toute_";
-    }
-    32% {
-        content: "42, c'est la réponse à tout_";
-    }
-    33% {
-        content: "42, c'est la réponse à tou_";
-    }
-    34% {
-        content: "42, c'est la réponse à _";
-    }
-    35% {
-        content: "42, c'est la réponse à_";
-    }
-    36% {
-        content: "42, c'est la réponse _";
-    }
-    37% {
-        content: "42, c'est la réponse_";
-    }
-    38% {
-        content: "42, c'est la répons_";
-    }
-    39% {
-        content: "42, c'est la répon_";
-    }
-    40% {
-        content: "42, c'est la répo_";
-    }
-    41% {
-        content: "42, c'est la rép_";
-    }
-    42% {
-        content: "42, c'est la ré_";
-    }
-    43% {
-        content: "42, c'est la r_";
-    }
-    44% {
-        content: "42, c'est la _";
-    }
-    45% {
-        content: "42, c'est l_";
-    }
-    46% {
-        content: "42, c'est _";
-    }
-    47% {
-        content: "42, c'est_";
-    }
-    48% {
-        content: "42, c'es_";
-    }
-    49% {
-       content: "42, c'e_";
-    }
-    50% {
-        content: "42, c'_";
-    }
-    51% {
-        content: "42, _";
-    }
-    52% {
-        content: "42,_";
-    }
-    53% {
-        content: "42_";
-    }
-    54% {
-        content: "4_";
-    }
-    55% {
-        content: "_";
-    }
-`
-
-const Animation3 = keyframes`
-0% {
-        content: "_";
-    }
-    2% {
-        content: "A_";
-    }
-    4% {
-        content: "Al_";
-    }
-    6% {
-        content: "Alo_";
-    }
-    8% {
-        content: "Alor_";
-    }
-    10% {
-        content: "Alors_";
-    }
-    12% {
-        content: "Alors,_";
-    }
-    14% {
-        content: "Alors, p_";
-    }
-    16% {
-        content: "Alors, po_";
-    }
-    18% {
-        content: "Alors, pou_";
-    }
-    20% {
-        content: "Alors, pour_";
-    }
-    22% {
-        content: "Alors, pourq_";
-    }
-    24% {
-        content: "Alors, pourqu_";
-    }
-    26% {
-        content: "Alors, pourquo_";
-    }
-    28% {
-        content: "Alors, pourquoi_";
-    }
-    30% {
-        content: "Alors, pourquoi n_";
-    }
-    32% {
-        content: "Alors, pourquoi ne_";
-    }
-    34% {
-        content: "Alors, pourquoi ne p_";
-    }
-    36% {
-        content: "Alors, pourquoi ne pa_";
-    }
-    38% {
-        content: "Alors, pourquoi ne pas_";
-    }
-    40% {
-        content: "Alors, pourquoi ne pas f_";
-    }
-    42% {
-        content: "Alors, pourquoi ne pas fa_";
-    }
-    44% {
-        content: "Alors, pourquoi ne pas fai_";
-    }
-    46% {
-        content: "Alors, pourquoi ne pas fair_";
-    }
-    48% {
-        content: "Alors, pourquoi ne pas faire_";
-    }
-    50% {
-        content: "Alors, pourquoi ne pas faire c_";
-    }
-    52% {
-        content: "Alors, pourquoi ne pas faire co_";
-    }
-    54% {
-        content: "Alors, pourquoi ne pas faire con_";
-    }
-    56% {
-        content: "Alors, pourquoi ne pas faire conf_";
-    }
-    58% {
-        content: "Alors, pourquoi ne pas faire confi_";
-    }
-    60% {
-        content: "Alors, pourquoi ne pas faire confia_";
-    }
-    62% {
-        content: "Alors, pourquoi ne pas faire confian_";
-    }
-    64% {
-        content: "Alors, pourquoi ne pas faire confiance_";
-    }
-    66% {
-        content: "Alors, pourquoi ne pas faire confiance à_";
-    }
-    68% {
-        content: "Alors, pourquoi ne pas faire confiance à 4_";
-    }
-    70% {
-        content: "Alors, pourquoi ne pas faire confiance à 42_";
-    }
-    72% {
-        content: "Alors, pourquoi ne pas faire confiance à 42d_";
-    }
-    74% {
-        content: "Alors, pourquoi ne pas faire confiance à 42de_";
-    }
-    76% {
-        content: "Alors, pourquoi ne pas faire confiance à 42dev_";
-    }
-    78% {
-        content: "Alors, pourquoi ne pas faire confiance à 42dev p_";
-    }
-    80% {
-        content: "Alors, pourquoi ne pas faire confiance à 42dev po_";
-    }
-    82% {
-        content: "Alors, pourquoi ne pas faire confiance à 42dev pou_";
-    }
-    84% {
-        content: "Alors, pourquoi ne pas faire confiance à 42dev pour_";
-    }
-    86% {
-        content: "Alors, pourquoi ne pas faire confiance à 42dev pour v_";
-    }
-    88% {
-        content: "Alors, pourquoi ne pas faire confiance à 42dev pour vo_";
-    }
-    90% {
-        content: "Alors, pourquoi ne pas faire confiance à 42dev pour vot_";
-    }
-    92% {
-        content: "Alors, pourquoi ne pas faire confiance à 42dev pour votre_";
-    }
-    94% {
-        content: "Alors, pourquoi ne pas faire confiance à 42dev pour votre s_";
-    }
-    96% {
-        content: "Alors, pourquoi ne pas faire confiance à 42dev pour votre si_";
-    }
-    98% {
-        content: "Alors, pourquoi ne pas faire confiance à 42dev pour votre sit_";
-    }
-    100% {
-        content: "Alors, pourquoi ne pas faire confiance à 42dev pour votre site web?_";
-    }
-`
-
-const Animation4 = keyframes`
-    0% {
-        opacity: 1;
-        content: "Alors, pourquoi ne pas faire confiance à 42dev pour votre site web?_";
-    }
-    100% {
-        opacity: 0;
-        content: "";
-    }
-`
-
-const Animation5 = keyframes`
-    0% {
-        opacity: 0;
-        transform: scale(0.5)
-    }
-    100% {
-        opacity: 1;
-        transform: scale(1)
-    }
-`
-
-const Main = styled.div`
+const QuestionContainer = styled.div`
+    position: absolute;
     background-color: #02001b;
-    height: 100vh;
-    width: 100vw;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: 1000;
     top: 0;
-    left: 0;
-    z-index: 1;
-    overflow: hidden;
-    `
+`
 
-const TextA1 = styled.span`
-    display: block;
-    font-size: 2.3vw;
-    color: #fff;
-    font-weight: 600;
-    font-family: 'Montserrat', sans-serif;
-    position: relative;
-    width: 48vw;
-    text-align: center;
-    margin: 50% auto;
-    z-index: 1;
-    &:before{
-        content: "_";
-        animation:  ${Animation1} 8.5s linear, ${Animation2} 5.5s linear 8.5s, ${Animation3} 7.5s linear 12.5s , ${Animation4} 2s linear 20s;
-        animation-fill-mode: forwards;
+const Question1 = styled.div`
+    position: absolute;
+    top: 11vw;
+    left: 5vw;
+    font-size: 1.7vw;
+    background-color: ${theme.primary};
+    width: 38vw;
+    padding: 3vw 2vw;
+    border-radius: 1.5vw;
+`
+
+const Question2 = styled.div`
+    position: absolute;
+    bottom: 11vw;
+    right: 5vw;
+    font-size: 1.7vw;
+    background-color: ${theme.primary};
+    width: 38vw;
+    padding: 3vw 2vw;
+    border-radius: 1.5vw;
+`
+
+const HeaderSection = styled.div`
+    display: flex;
+    align-items: center;
+    width: 95%;
+    background-color: ${theme.primary};
+    height: 8vw;
+    border-radius: 50px;
+    padding-left: 3vw;
+    margin-left: 50%;
+    margin-top: 5vw;
+    transform: translateX(-50%);
+    img{
+        width: 7vw;
+        margin-right: 3vw;
+    }
+    h1{
+        font-size: 1.5vw;
+        margin-bottom: 0vw;
+    }
+    p{
+        font-size: 1vw;
+        margin-top: 0.6vw;
     }
 `
 
-const ImgLogo = styled.img`
-    animation: ${Animation5} 1s linear 20s;
-    width: 10vw;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    opacity: 0;
-    z-index: 2;
-    transform: translate(-50%, -50%);
+const InfoConteneur = styled.div`
+    margin-top: 1vw;
+    margin-bottom: 5vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `
 
-// const Header = styled.div`
-//     display: flex;
-//     align-items: center;
-//     margin-top: 5vw;
-//     width: 95%;
-//     background-color: ${theme.primary};
-//     height: 8vw;
-//     border-radius: 50px;
-//     padding-left: 3vw;
-//     margin-left: 50%;
-//     transform: translateX(-50%);
-//     img{
-//         width: 7vw;
-//         margin-right: 3vw;
-//     }
-// `
+const Info = styled.div`
+    background-color: ${theme.secondary};
+    border-radius: 50px;
+    width: 95%;
+    height: 19vw;
+`
 
-// const InfoConteneur = styled.div`
-//     margin-top: 1vw;
-//     margin-bottom: 5vw;
-//     display: flex;
-//     flex-direction: column;
-//     align-items: center;
-// `
+const InfoHover = styled.div`
+    position: absolute; 
+    right: 0;
+    height: 19vw;
+    background-color: ${theme.primary};
+    width: 49.5%;
+    border-radius: 50px 0 0 50px;
+    box-shadow: -8px 10px 7px 0px rgba(0, 0, 0, 0.25);
+    h2{
+        margin: 0;
+    }
+`
 
-//  const Info = styled.div`
-//     background-color: ${theme.secondary};
-//     border-radius: 50px;
-//     width: 95%;
-//     height: 19vw;
-// `
-
-// const InfoHover = styled.div`
-//     position: absolute; 
-//     right: 0;
-//     height: 19vw;
-//     background-color: ${theme.primary};
-//     width: 49.5%;
-//     border-radius: 50px 0 0 50px;
-//     box-shadow: -8px 10px 7px 0px rgba(0, 0, 0, 0.25);
-//     h2{
-//         margin: 0;
-//     }
-// `
-
-// const InfoHover2 = styled.div`
-//     position: absolute;
-//     left: 0;
-//     height: 19vw;
-//     background-color: ${theme.primary};
-//     width: 49.5%;
-//     border-radius:  0 50px 50px 0 ;
-//     box-shadow: 8px 0px 7px 0px rgba(0, 0, 0, 0.25);
-//     h2{
-//         margin: 0;
-//     }
-// `
+const InfoHover2 = styled.div`
+    position: absolute;
+    left: 0;
+    height: 19vw;
+    background-color: ${theme.primary};
+    width: 49.5%;
+    border-radius:  0 50px 50px 0 ;
+    box-shadow: 8px 0px 7px 0px rgba(0, 0, 0, 0.25);
+    h2{
+        margin: 0;
+    }
+`
 
 export default function A_TechPro() {
 
-  
+    useEffect(() => {
+        gsap.timeline({ defaults: { ease: "power1.out" } })
+            .fromTo(
+                [".Question1", ".Question2"], // Assurez-vous que les classes correspondent
+                { opacity: 0, y: 50 },
+                { opacity: 1, y: 0, duration: 0.7, stagger: 1.5 }
+            )
+            .to(".QuestionContainer", { opacity: 0, duration: 0.7, delay: 4 , display: "none" });
+    }, []);
 
     return (
-        <Container>
+        <Container >
 
-            {/* <Header>
+            <QuestionContainer className="QuestionContainer">
+                <Question1 className="Question1">
+                    <span>
+                        42, c'est la réponse à toutes les questions, n'est-ce pas?
+                    </span>
+                </Question1>
+                <Question2 className="Question2">
+                    <span>
+                        Alors, pourquoi ne pas faire confiance à 42dev pour votre site web?
+                    </span>
+                </Question2>
+            </QuestionContainer>
+
+            <HeaderSection>
                 <img src={logo_42dev} />
                 <div>
                     <h1>42dev</h1>
                     <p>Création de sites internet sur mesure, adaptés à vos besoins et à votre budget.</p>
                 </div>
-            </Header>
+            </HeaderSection>
 
             <InfoConteneur>
-
                 <Info>
                     <div></div>
                     <InfoHover>
@@ -664,13 +161,7 @@ export default function A_TechPro() {
                         <h2>Création de sites internet</h2>
                     </InfoHover2>
                 </Info>
-
-            </InfoConteneur> */}
-
-            <Main>
-                <TextA1></TextA1>
-                <ImgLogo src={logo_42dev} />
-            </Main>
+            </InfoConteneur>
 
         </Container>
     )
